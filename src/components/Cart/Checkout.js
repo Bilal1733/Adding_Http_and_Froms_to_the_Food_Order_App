92e723b33b4c107520046f9d92bdfd7d6abadbce
bilals <bks1733@gmail.com>
2023-04-18 14:39:40 +0530

    Adding A Checkout Form.

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import classes from './Checkout.module.css';
+
+const Checkout = (props) => {
+  const confirmHandler = (event) => {
+    Event.PREVENTdEFAULT();
+  };
+
+  return (
+    <form onSubmit={confirmHandler}>
+      <div className={classes.control}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" name="" id="name" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="street">Street</label>
+        <input type="text" name="" id="street" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" name="" id="postal" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="city">City</label>
+        <input type="text" name="" id="city" />
+      </div>
+      <button type="button" onClick={props.onCancel}>
+        Cancel
+      </button>
+      <button>Confirm</button>
+    </form>
+  );
+};
+
+export default Checkout;
